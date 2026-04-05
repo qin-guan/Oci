a0f216b16f892c3c9661fa08a572e1084f39d761
Qin Guan <qinguan20040914@gmail.com>
2026-04-05 14:49:04 +0800

    Add compute instance

--- Oci/Stacks/DefaultStack.cs
+++ Oci/Stacks/DefaultStack.cs
@@ -1,4 +1,5 @@
 ﻿using Pulumi;
+using Pulumi.Oci.Core;
 
 namespace Oci.Stacks;
 
@@ -6,6 +7,62 @@ public class DefaultStack : Stack
 {
     public DefaultStack()
     {
+        var config = new Config();
+        var compartmentOcid = config.Require("compartmentOcid");
+        var vcnCidrBlock = config.Require("vcnCidrBlock");
+        var subnetCidrBlock = config.Require("subnetCidrBlock");
+        var ubuntu24ImageOcid = config.Require("ubuntu24ImageOcid");
 
+        var vcn = new Vcn(
+            "temasek3-prod-vcn",
+            new VcnArgs
+            {
+                CompartmentId = compartmentOcid,
+                CidrBlock = vcnCidrBlock,
+                DisplayName = "temasek3-prod-vcn",
+            }
+        );
+
+        var subnet = new Subnet(
+            "temasek3-prod-subnet",
+            new SubnetArgs
+            {
+                CompartmentId = compartmentOcid,
+                VcnId = vcn.Id,
+                CidrBlock = subnetCidrBlock,
+                DisplayName = "temasek3-prod-subnet",
+            }
+        );
+
+        var instance = new Instance(
+            "temasek3-prod",
+            new InstanceArgs
+            {
+                CompartmentId = compartmentOcid,
+                AvailabilityDomain = "YAbW:AP-SINGAPORE-1-AD-1",
+                DisplayName = "temasek3-prod",
+                Shape = "VM.Standard.A1.Flex",
+
+                ShapeConfig = new Pulumi.Oci.Core.Inputs.InstanceShapeConfigArgs
+                {
+                    Ocpus = 1,
+                    MemoryInGbs = 4,
+                },
+
+                CreateVnicDetails = new Pulumi.Oci.Core.Inputs.InstanceCreateVnicDetailsArgs
+                {
+                    SubnetId = subnet.Id,
+                },
+
+                SourceDetails = new Pulumi.Oci.Core.Inputs.InstanceSourceDetailsArgs
+                {
+                    SourceType = "image",
+                    SourceId = ubuntu24ImageOcid,
+                    BootVolumeSizeInGbs = "32",
+                },
+
+                FreeformTags = { { "managed-by", "pulumi" } },
+            }
+        );
     }
 }
